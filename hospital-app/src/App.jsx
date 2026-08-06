fc2119ac0d4035a7386761ccf15fee46d6a23d98
tetumer <tetumeruday@gmail.com>
2026-08-07 00:16:34 +0600

    Add Login route to App component

--- hospital-app/src/App.jsx
+++ hospital-app/src/App.jsx
@@ -11,23 +11,25 @@ import LabReports from './LabReports'
 import Dashboard from './Dashboard'
 import Bill from './Bill'
 import Reception from './Reception'
+import Login from './Login'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/patients" element={<PatientList />} />
-        <Route path="/doctors" element={<DoctorList/>}/>
+        <Route path="/doctors" element={<DoctorList />} />
         <Route path="/patients/:id" element={<PatientDetails />} />
-        <Route path="/doctors/:id" element={<DoctorDetails/>} />
-        <Route path="/appointments" element={<Appointments/>} />
-        <Route path="/departments" element={<DepartmentList/>} />
-        <Route path="/departments/:id" element={<DepartmentDetails/>} />      
-        <Route path="/prescriptions" element={<PrescriptionList/>} /> 
-        <Route path="/labreports" element = {<LabReports/>} />  
-        <Route path="/dashboard" element = {<Dashboard/>} /> 
-        <Route path="/bill" element = {<Bill/>} />
-        <Route path="/reception" element={<Reception/>} />
+        <Route path="/doctors/:id" element={<DoctorDetails />} />
+        <Route path="/appointments" element={<Appointments />} />
+        <Route path="/departments" element={<DepartmentList />} />
+        <Route path="/departments/:id" element={<DepartmentDetails />} />
+        <Route path="/prescriptions" element={<PrescriptionList />} />
+        <Route path="/labreports" element={<LabReports />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/bill" element={<Bill />} />
+        <Route path="/reception" element={<Reception />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
