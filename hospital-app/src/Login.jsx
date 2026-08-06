cb97967da95b73a113ab55e892c046f3ea59b0f1
tetumer <tetumeruday@gmail.com>
2026-08-06 19:14:28 +0600

    Implement login functionality in Login component
    Added handleLogin function to manage user login and handle responses.

--- hospital-app/src/Login.jsx
+++ hospital-app/src/Login.jsx
@@ -6,7 +6,27 @@ function Login(){
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
-    
+    const handleLogin = () => {
+        console.log("Trying login with:", username, password)
+        fetch('https://localhost:7172/api/user/login', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ Username: username, Password: password, Role: "" })
+        })
+            .then((response) => {
+            if (!response.ok) {
+                throw new Error('Invalid username or password')
+            }
+            return response.json()
+            })
+            .then((user) => {
+            console.log('Logged in as:', user)
+
+            })
+            .catch((error) => {
+            alert(error.message)
+            })
+        }
 
     return(
         <div>
@@ -20,7 +40,7 @@ function Login(){
                 <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
             <div>
-                <button>Login</button>
+                <button onClick={handleLogin}>Login</button>
             </div>
         </div>
     )
